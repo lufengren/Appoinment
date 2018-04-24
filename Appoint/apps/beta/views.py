92c653833421072805746dcf3dbd5a34a9a657c9
Xiaojing Liu <iceliuxj@gmail.com>
2018-04-23 21:08:15 -0700

    fixed merge issue

--- Appoint/apps/beta/views.py
+++ Appoint/apps/beta/views.py
@@ -39,9 +39,5 @@ def login(request):
         return redirect('/main')  
 
 def mainpage(request):
-<<<<<<< Updated upstream
     return render(request, 'beta/mainpage.html')
     
-=======
-    return render(request, 'beta/mainpage.html')
->>>>>>> Stashed changes
